7b6b52a2b86c13ce12b8b4a99e5e82b841c3fcbb
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-20 01:50:33 +0300

    Add navbar

---
+++ frontend/src/components/PortfolioNavbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="portfolio-navbar">
+    <vs-navbar color="#000000" square v-model="active">
+      <template #left>
+        <vs-navbar-item class="peepologo">
+          <img
+            class="greet-pic"
+            src="https://c.tenor.com/dyoCFHNiIAwAAAAM/peepo-hey-peepo.gif"
+            alt=""
+          /> </vs-navbar-item
+      ></template>
+      <template #right>
+        <vs-navbar-item :active="active == 'home'" id="guide">
+          Home
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          Documents
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'components'" id="components">
+          Components
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'license'" id="license">
+          license
+        </vs-navbar-item></template
+      >
+      <!-- <template #right></template> -->
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { active: "home" };
+  },
+};
+</script>
+
+<style>
+.peepologo {
+}
+.greet-pic {
+  width: 20%;
+}
+.square {
+  margin-top: 50px;
+}
+</style>
