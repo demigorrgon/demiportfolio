837d3c0c8c54de8f203b352e368bea44ac32c3d9
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-20 11:25:47 +0300

    Link new dependencies to project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuesax from 'vuesax'
-
+import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd'
 import 'vuesax/dist/vuesax.css'
+import { uniGithub } from 'vue-unicons/dist/icons'
+import vuetify from './plugins/vuetify'
+
+Unicon.add([uniGithub])
+
 Vue.config.productionTip = false
 Vue.use(Vuesax)
+Vue.use(Unicon)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
