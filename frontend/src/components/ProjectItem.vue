8bb40981263448ab06546a89aa53cce9f945711f
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-20 11:26:42 +0300

    Update single project info component

--- frontend/src/components/ProjectItem.vue
+++ frontend/src/components/ProjectItem.vue
@@ -1,28 +1,44 @@
 <template>
   <div class="project">
     <vs-row>
-      <vs-col w="4" style="border: 1px solid red;margin-bottom:20px;">
+      <vs-col w="4" style="border: 1px solid red; margin-bottom: 20px">
         <vs-card type="4" class="project-card">
           <template #title>
-            <h3>Pot with a plant</h3>
+            <h3>{{ title }}</h3>
           </template>
           <template #img>
             <img :src="require('@/assets/' + pictureName)" alt="" />
           </template>
           <template #text>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            <p>{{ techStackUsed }}</p>
           </template>
           <template #interactions>
+            <vs-button
+              color="rgb(255,255,255)"
+              border
+              icon
+              @click="goToGithubRepo"
+            >
+              <a :href="githubLink" target="_blank">
+                <unicon name="github"></unicon
+              ></a>
             </vs-button>
           </template>
         </vs-card>
       </vs-col>
       <vs-col w="7" offset="1" style="border: 1px solid red">
-        <p>{{title}}</p>
-        <p>{{description}}</p>
-        <ul v-for="(item, idx) of featuresArray" :key="idx" class="features-list">
-          <li>{{item}}</li>
-        </ul>
+        <p>{{ title }}</p>
+        <p class="project-description">{{ description }}</p>
+        <div class="features">
+          <p>Features:</p>
+          <ul
+            v-for="(item, idx) of featuresArray"
+            :key="idx"
+            class="features-list"
+          >
+            <li>{{ item }}</li>
+          </ul>
+        </div>
       </vs-col>
     </vs-row>
   </div>
@@ -30,7 +46,19 @@
 
 <script>
 export default {
-  props: ["title", "description", "pictureName", "featuresArray"],
+  props: [
+    "title",
+    "techStackUsed",
+    "description",
+    "pictureName",
+    "featuresArray",
+    "githubLink",
+  ],
+  methods: {
+    goToGithubRepo() {
+      window.open(url, "_blank").focus();
+    },
+  },
 };
 </script>
 
@@ -38,8 +66,13 @@ export default {
 .project-card {
   margin-left: 100px;
 }
+
 .features-list {
-  padding: 0;
-  list-style-type: none;
+  padding: 0px;
+  margin: 5px 0 5px 0;
+  list-style-type: None;
+}
+.project-description {
+  padding: 10px 30px 10px 30px;
 }
 </style>
