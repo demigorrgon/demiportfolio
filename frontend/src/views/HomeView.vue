d239b630a5514ae33cceea7acbfc9b43033d60b5
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-20 11:26:49 +0300

    Update home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,17 +5,21 @@
       Here is the list of the projects that I've done in past 6 months.<br />
       They're clickable, don't be scared.
     </p>
+
     <project-item
-      title="some"
-      description="lorem"
+      title="some cool project idk"
+      techStackUsed="Backend: Django/Django Rest Framework, PostgreSQL. Frontend: VueJS, Vuex. Styling: Vuesax 3. Containerized with Docker"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vero, adipisci exercitationem quis velit aliquid, reprehenderit, perferendis quam dignissimos corporis facere tempora magnam! Quam aspernatur quos reprehenderit velit vero provident rerum asperiores, illo cumque. Aspernatur at minima odio sed ullam cumque aperiam impedit corporis accusamus. Delectus cupiditate nam quisquam fuga sapiente ipsa minima consequatur repellendus, animi voluptatem quas, aspernatur nesciunt possimus. Corporis nihil at totam voluptas quod iure quisquam non, illum voluptates itaque cum ipsum maxime vero quas vel ratione? Corrupti facilis doloribus reiciendis harum odit molestias possimus suscipit. Animi, recusandae enim iure ex placeat sapiente et libero officiis."
       pictureName="github-pic.png"
       :featuresArray="['asd', 'qwe']"
     />
     <project-item
       title="some"
-      description="lorem"
+      techStackUsed="Backend: Vanilla Flask, PostgreSQL. Frontend: VueJS, Vuex. Styling: Vuesax 3. Containerized with Docker"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vero, adipisci exercitationem quis velit aliquid, reprehenderit, perferendis quam dignissimos corporis facere tempora magnam! Quam aspernatur quos reprehenderit velit vero provident rerum asperiores, illo cumque. Aspernatur at minima odio sed ullam cumque aperiam impedit corporis accusamus. Delectus cupiditate nam quisquam fuga sapiente ipsa minima consequatur repellendus, animi voluptatem quas, aspernatur nesciunt possimus. Corporis nihil at totam voluptas quod iure quisquam non, illum voluptates itaque cum ipsum maxime vero quas vel ratione? Corrupti facilis doloribus reiciendis harum odit molestias possimus suscipit. Animi, recusandae enim iure ex placeat sapiente et libero officiis."
       pictureName="github-pic.png"
       :featuresArray="['asd', 'qwe']"
+      githubLink="https://github.com/demigorrgon/el-shopperino-dj-vue"
     />
   </div>
 </template>
