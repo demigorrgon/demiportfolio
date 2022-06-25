eec11dcb37337499daeccc0fbaa8376bb5950483
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-25 23:37:42 +0300

    Add trusted origins (apparently some django 4.0 thing)

--- backend/twitter_project/twitter_clone/settings.py
+++ backend/twitter_project/twitter_clone/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "twitter_clone.urls"
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "https://*.127.0.0.1"]
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -126,11 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = ""
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join("static"),)
+# STATICFILES_DIRS = (os.path.join("twitter/static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
