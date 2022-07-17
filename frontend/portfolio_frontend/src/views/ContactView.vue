3004a12f240168fe53c739ee109f1bb0b690e2f6
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-17 21:17:33 +0300

    Update cv part

--- frontend/portfolio_frontend/src/views/ContactView.vue
+++ frontend/portfolio_frontend/src/views/ContactView.vue
@@ -62,31 +62,14 @@
           vs-justify="center"
           vs-align="center"
         >
-          <p>Summary</p>
+          <p>Profile</p>
           <hr />
           <p class="a-lot-of-text">
-            Self-taught Python developer with passion to build services for
-            humans. Most comfortable with Django/DRF/Flask frameworks, using
-            PostgreSQL as default DB at almost all times, usually in Docker
-            containers to simplify switching process between dev/test/prod
-            environments of different projects. Certified expert in hiding external API keys and other configs in .env file and forgetting that your teammates might also need them
+            &emsp;Self-taught back-end developer with passion to build services for humans, with more than a year of experience using Python and it's rich infrastructure 
             <br>
+            &emsp;Washed up candidate master in chess.
             <br>
-            Willing to switch to any NoSQL DB in case of need.
-            <br>
-            <br>
-            This website was deployed with great help of docker-compose, nginx image in Docker and AWS EC2 service.
-            <br>
-            <br>
-            Usually unittesting stuff with pytest. It's just great.
-            <br>
-            <br>
-            Sometimes doing JS(VueJS specifically) stuff for fun, it does give
-            some vibe of "I'm typing a lot of relatively meaningless code so I
-            definitely look like 1337hackerman"
-            <br>
-            <br>
-            Fueled by memes, videos, streams and bunch of Spotify playlists
+            &emsp;Certified expert in hiding external API keys and other configs in .env file and forgetting that your teammates might also need them &emsp;/s   
           </p>
 
           <p>Languages</p>
@@ -96,6 +79,7 @@
             <li>
               English -
               <a
+              class="profile-links"
                 href="https://www.efset.org/cert/AjUBNV"
                 title="Link to certificate"
                 >low C2</a
@@ -110,11 +94,12 @@
             2020 - terminated due to major health issues.
             <br />
             <p class="prog">Notable progress during the internship:</p>
-            <p class="what-learned">- First expirience in developing REST APIs with Flask. Sadly, had to withdraw due to multiple surgeries and quite a long recovery period.</p>
+            <p class="what-learned">- First experience in developing REST APIs with Flask. Sadly, had to withdraw due to multiple surgeries and quite a long recovery period.</p>
 
           </p>
+          <hr style="opacity: 0.2">
           <p class="a-lot-of-text">
-            <a href="https://dou.ua/calendar/42418/" target="_blank"
+            <a class="profile-links" href="https://dou.ua/calendar/42418/" target="_blank"
               >Smarttek Academy Python Web Development</a
             >
             - Unpaid Internship: February 2022 - April 2022 - program terminated
@@ -122,59 +107,73 @@
             <p class="prog">Notable progress during the internship:</p>
             <p class="what-learned">- Was initially appointed into a group of ppl who'd be going into paid intership program, but litterally next day after the interview mr.putin decided to liberate me from my first money made as dev /shrug</p>
             <p class="what-learned">- Was leading a team of 4 devs in SCRUM environment, that shifted a bit into KANBAN and became a mix of both, really </p>
-            <p class="what-learned">- Aforementioned project was an DRF/Vue thematic freelance board dedicated to find freelance people with knowledge and skills in building homes/bridges/roads etc. As ironic as it might sound, development of it died after the announcement of the pause in the internship</p>
-            <p class="what-learned">- In general, it was a first expirience in working as a team, actually testing stuff with unittests, understanding how much pain setting up Gitlab CI is/was. And a lot of other fancy stuff. </p>
+            <p class="what-learned">- Aforementioned project was an DRF/Vue thematic freelance board dedicated to find freelance people with knowledge and skills in building homes/bridges/roads etc. As ironic as it might sound, development of it died after the announcement of the pause in the internship because of the war</p>
+            <p class="what-learned">- In general, it was a first experience in working as a team, actually unittesting code, planning and communicating through issues. </p>
+            <p class="what-learned">- Was responsible for building a chat app with the help of <a class="profile-links" href="https://channels.readthedocs.io/en/stable/" target="_blank">django-channels</a>; CI pipeline setup in Gitlab and other usual stuff</p>
+
 
           </p>
+          <hr style="opacity: 0.2">
           <p class="a-lot-of-text">
-            <a href="https://dou.ua/calendar/42123/">Coxit Academy Python</a> -
+            <a class="profile-links" href="https://dou.ua/calendar/42123/">Coxit Academy Python</a> -
             Unpaid Internship: May 2022 - current
-            <p class="prog">Progress during the internship:</p>
-            <p class="what-learned">- <a href="" target="_blank">IJRA</a> was initially a task of 'yo, create some CRUD API' which went a bit too south</p>
+            <p class="prog">Notable progress during the internship:</p>
+            <p class="what-learned">- <a class="profile-links" href="http://demigorrgon.design/ijra/" target="_blank">IJRA</a> was initially a task of 'yo, create some CRUD API' which went a bit too south</p>
             <p class="what-learned">- Was responsible for implementing somewhat of a frontend for an internal application, that was using Slack API and it's Block Kit in Django application, which was a frontend app. Don't ask me why.</p>
-            <p class="what-learned">- Learned about API mocking in tests (on top of basic unittests), which actually helped quite a bit in testing that awful Slack's API. Trust me, it <i>really</i> is.</p>
+            <p class="what-learned">- Learned about API mocking in tests (on top of basic unittests), which actually helped quite a bit in testing Slack's API.</p>
             <p class="what-learned">- Those sick bastards led me into using Ubuntu(Linux) as main OS.<br> <b>/s</b> just in case</i>.</p>
             <br />
           </p>
+          <hr style="opacity: 0.2">
           <p class="a-lot-of-text">
-            <a href="https://dou.ua/calendar/42418/" target="_blank"
+            <a class="profile-links" href="https://dou.ua/calendar/42418/" target="_blank"
               >Smarttek Academy Python Web Development</a
             >
             - Unpaid Internship: June 2022 - current <br />
             - program continued and goes in parallel with Coxit Academy
             <br />
             <p class="prog">Notable progress during the internship:</p>
-            <p class="what-learned">- Was participating in <a href="http://hariart.org/" target="_blank">Hariart</a> project. Added celery worker for delayed emails sending.</p>
-            <p class="what-learned">- Cleaned up dockerizing process in aforementioned project, learned about image hashing, reminded myself how to scrape pictures from websites in legal way.</p>
+            <p class="what-learned">- Was participating in <a class="profile-links" href="http://hariart.org/" target="_blank">Hariart</a> project's development.</p> 
+            <p class="what-learned">&emsp;- Was responsible for adding <a class="profile-links" href="https://docs.celeryq.dev/en/stable/" target="_blank">celery</a> worker for delayed emails sending.</p>
+            <p class="what-learned">&emsp;- Cleaned up dockerizing process in aforementioned project, learned about image hashing, reminded myself how to scrape pictures from websites in legal way.</p>
           </p>
 
-          <p style="margin-top:30px">Tech stack/Portfolio:</p>
+          <p style="margin-top:30px">Tech stack:</p>
           <hr />
           <p>
             <vs-row>
-              <vs-col w="1">
-                <img
-                  src="https://www.streamscheme.com/wp-content/uploads/2020/09/monkahmm-emote.png"
-                  class="monkaHmm"
-                />
-              </vs-col>
-              <vs-col w="6" style="margin: 10px 0px 0px 10px">
-                <router-link to="/"
-                  ><p style="color: black">
-                    Where potentially could it be?
-                  </p></router-link
-                ></vs-col
-              >
+              <vs-col><p class="">Backend related:</p></vs-col>
+              <vs-col><p class="what-learned">- Python 3.6+</p></vs-col>
+              <vs-col><p class="what-learned">- Django/Django REST Framework</p></vs-col>
+              <vs-col><p class="what-learned">- Flask (Flask-SQLAlchemy, Flask-Marshmallow, Flask-Migrate)</p></vs-col>
+              <vs-col><p class="">Databases:</p></vs-col>
+              <vs-col><p class="what-learned">- PostgreSQL</p></vs-col>
+              <vs-col><p class="what-learned">- sqlite</p></vs-col>
+              <vs-col><p >Scraping:</p></vs-col>
+              <vs-col><p class="what-learned">- Selenium (dynamic websites)</p></vs-col>
+              <vs-col><p class="what-learned">- requests + beautifulsoup4 (static websites)</p></vs-col>
+              <vs-col><p >Testing:</p></vs-col>
+              <vs-col><p class="what-learned">- pytest</p></vs-col>
+              <vs-col><p class="what-learned">- DRF test client/factory for API testing</p></vs-col>
+              <vs-col><p class="what-learned">- Postman (manual)</p></vs-col>
+              <vs-col><p >DevOps related:</p></vs-col>
+              <vs-col><p class="what-learned">- Docker/docker-compose</p></vs-col>
+              <vs-col><p class="what-learned">- nginx as a docker-compose service, basic configs etc</p></vs-col>
+              <vs-col><p class="what-learned">- Github Actions/Gitlab runner setups </p></vs-col>
+              <vs-col><p >Frontend related:</p></vs-col>
+              <vs-col><p class="what-learned">- Basic ES5/ES6 Javascript syntax, axios</p></vs-col>
+              <vs-col><p class="what-learned">- Basic vuejs (lifecycle hooks, 'v-' keywords)</p></vs-col>
+              <vs-col><p class="what-learned">- HTML5, vuetify, vuesax styling frameworks</p></vs-col>
             </vs-row>
           </p>
           <p>Education:</p>
           <hr />
           <p class="a-lot-of-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            voluptates. Impedit facilis deserunt obcaecati iste temporibus
-            molestias dignissimos voluptatem, quia perferendis totam iure sed
-            debitis, atque earum eaque voluptas ab.
+            &emsp;Lviv Polytechnic National University, Bachelor's degree: Automation and computer
+integrated technologies, 2017-2021. Main technologies used: MATLAB, Simulink
           </p>
+          <p class="a-lot-of-text">&emsp;Lviv Polytechnic National University, Master's degree: Automation and computer
+integrated technologies, 2021-current. Main technologies used: MATLAB, Simulink</p>
           <p>Nerdy stuff:</p>
           <hr />
           <p class="external-links">
@@ -182,7 +181,6 @@
               ><p>Codewars profile</p></a
             >
 
-            <br />
             <a href="https://leetcode.com/demigorrgon/"
               ><p>Leetcode profile</p></a
             >
@@ -241,6 +239,7 @@ ul {
   width: 100%;
 }
 .external-links {
+  margin-top: 15px !important;
   margin-bottom: 30px;
 }
 .external-links a {
@@ -253,5 +252,12 @@ ul {
 .what-learned {
   padding-left: 30px;
   font-weight: 300 !important;
+}
+.profile-links {
+  text-decoration: none;
+  color: #0000ee;
+}
+.profile-links:hover {
+  text-decoration: underline;
 }
 </style>
